7a843c9035a0a49043fbc751ac67ba796709cfed
Tori Gushulak <tori.gushulak@gmail.com>
2023-04-22 22:39:22 -0600

    done 3D text

--- src/script.js
+++ src/script.js
@@ -1,6 +1,8 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js'
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js'
 
 /**
  * Base
@@ -14,10 +16,72 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
+// axes helper
+// const axesHelper = new THREE.AxesHelper()
+// scene.add(axesHelper)
+
 /**
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
+
+const matcapTexture = textureLoader.load('/textures/matcaps/8.png')
+
+// fonts
+const fontLoader = new FontLoader();
+fontLoader.load(
+    '/fonts/helvetiker_regular.typeface.json', 
+    (font) => {
+        const textGeometry =  new TextGeometry(
+            'Hello Three.js', {
+                font: font,
+                size: 0.5,
+                height: 0.2,
+                curveSegments: 5,
+                bevelEnabled: true,
+                bevelThickness: 0.03,
+                bevelSize: 0.02,
+                bevelOffset: 0,
+                bevelSegments: 4
+            }
+        );
+
+        // centers text by the bounding box
+        // textGeometry.computeBoundingBox();
+        // textGeometry.translate(
+        //     - (textGeometry.boundingBox.max.x - 0.02) * 0.5,
+        //     - (textGeometry.boundingBox.max.y - 0.02) * 0.5,
+        //     - (textGeometry.boundingBox.max.z - 0.03) * 0.5
+        // )
+        textGeometry.center()
+
+        const material = new THREE.MeshMatcapMaterial({matcap: matcapTexture})
+        const text = new THREE.Mesh(textGeometry, material)
+        scene.add(text)
+
+
+        const donutGeometry = new THREE.TorusGeometry(0.3,0.3,20,45)
+        
+
+        for (let i = 0; i < 250; i++) {
+            
+            const donut = new THREE.Mesh(donutGeometry, material)
+
+            donut.position.x = (Math.random() - 0.5) * 10;
+            donut.position.y = (Math.random() - 0.5) * 10;
+            donut.position.z = (Math.random() - 0.5) * 10;
+
+            donut.rotation.x = Math.random() * Math.PI;
+            donut.rotation.y = Math.random() * Math.PI;
+
+            const scale = Math.random()
+            donut.scale.set(scale, scale, scale)
+
+            scene.add(donut)
+        }
+    }
+)
+
 
 /**
  * Object
@@ -27,7 +91,7 @@ const cube = new THREE.Mesh(
     new THREE.MeshBasicMaterial()
 )
 
-scene.add(cube)
+// scene.add(cube)
 
 /**
  * Sizes
